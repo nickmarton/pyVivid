6392aadb932b8de68efeb28cda200a4b2a30acf3
nickmarton <nickmarton1993@gmail.com>
2015-11-03 01:00:21 -0500

    done Vocabulary refactoring, setting up unit tests

--- Classes/Vocabulary.py
+++ Classes/Vocabulary.py
@@ -63,145 +63,56 @@ class Vocabulary:
             raise ValueError(
                 'C and V parameters may not have a common element')
 
-        self._C = list(set(C))
-        self._R = list(set(R))
-        self._V = list(set(V))
+        self._C = sorted(list(set(C)))
+        self._R = sorted(list(set(R)), key=lambda rs: rs._name)
+        self._V = sorted(list(set(V)))
 
     def __eq__(self, other):
-        """
-        Return boolean for whether or not this Vocabulary is equal 
-        to other; magic function used for easy equality i.e., 
-        self == other.
-        """
+        """Implement == operator for Vocabulary object."""
+        #constructor removes duplicates, so set comparison okay
+        c_cond = set(self._C) == set(other._C)
+        r_cond = set(self._R) == set(other._R)
+        v_cond = set(self._V) == set(other._V)
         
-        #try to find one-to-one mapping of self._r to other._r; only need to check one side as lengths are the same and no duplicates allowed
-
-        if len(self._r) == len(other._r):                                               #if R has the same length in both self and other
-            for r1 in self._r:                                                          #for each RelationSymbol in R of self
-                for r2 in other._r:                                                 #for each RelationSymbol in R of other
-                    if r1 == r2:                                                        #if r1 has a match in r2
-                        break                                                           #break from for-else
-                else:                                                                   #no match found for r1 in r2
-                    return False                                                        #not equal, return False
-        else:                                                                           #if R's have different length
-            return False                                                                #not equal, return False
-
-        r = True                                                                        #condition 2, r's match; this is checked above
-
-        if len(self._c) == len(other._c):                                               #if length of C's match
-            if set(self._c) == set(other._c):                                           #if unordered lists are equivalent
-                c = True                                                                #then C's match
-            else:                                                                       #otherwise no 1-to-1 mapping
-                c = False                                                               #C's don't match
-        else:                                                                           #C lengths aren't the same
-            c = False                                                                   #C's don't match
-
-        if len(self._v) == len(other._v):                                               #if length of V's match
-            if set(self._v) == set(other._v):                                           #if unordered lists are equivalent
-                v = True                                                                #then V's match
-            else:                                                                       #otherwise no 1-to-1 mapping
-                v = False                                                               #V's don't match
-        else:                                                                           #V lengths aren't the same
-            v = False                                                                   #V's don't match
-        
-        if c and r and v:                                                               #if all 3 conditions met, they are equal
-            return True                                                                 #return True
-        else:                                                                           #some component in self isn't equal to its counterpart in other
-            return False                                                                #return False
+        if c_cond and r_cond and v_cond:
+            return True
+        else:
+            return False
     
     def __ne__(self, other):
-        """
-        Return boolean for whether or not this Vocabulary is not 
-        equal to other; magic function used for easy equality i.e.,
-        self != other.
-        """
-        
-        #try to find one-to-one mapping of self._r to other._r; only need to check one side as lengths are the same and no duplicates allowed
+        """Implement != operator for Vocabulary object."""
+        return not self.__eq__(other)
 
-        if len(self._r) == len(other._r):                                               #if R has the same length in both self and other
-            for r1 in self._r:                                                          #for each RelationSymbol in R of self
-                for r2 in other._r:                                                 #for each RelationSymbol in R of other
-                    if r1 == r2:                                                        #if r1 has a match in r2
-                        break                                                           #break from for-else
-                else:                                                                   #no match found for r1 in r2
-                    return True                                                         #not equal, return True
-        else:                                                                           #if R's have differnet length
-            return True                                                                 #not equal, return True
-        
-        r = True                                                                        #condition 2, r's match; this is checked above
+    def __deepcopy__(self):
+        """Implement copy.deepcopy for Vocabulary object."""
+        from copy import deepcopy
+        return Vocabulary(deepcopy(c_copy), deepcopy(r_copy), deepcopy(v_copy))
 
-        if len(self._c) == len(other._c):                                               #if length of C's match
-            if set(self._c) == set(other._c):                                           #if unordered lists are equivalent
-                c = True                                                                #then C's match
-            else:                                                                       #otherwise no 1-to-1 mapping
-                c = False                                                               #C's don't match
-        else:                                                                           #C lengths aren't the same
-            c = False                                                                   #C's don't match
-
-        if len(self._v) == len(other._v):                                               #if length of V's match
-            if set(self._v) == set(other._v):                                           #if unordered lists are equivalent
-                v = True                                                                #then V's match
-            else:                                                                       #otherwise no 1-to-1 mapping
-                v = False                                                               #V's don't match
-        else:                                                                           #V lengths aren't the same
-            v = False                                                                   #V's don't match
-        
-        if not c or not r or not v:                                                     #some component in self isn't equal to its counterpart in other
-            return True                                                                 #return True
-        else:                                                                           #all components equal
-            return False                                                                #return False
-
-    def deep_copy(self):
-        """Return a deep copy of this Vocabulary object."""
-        import copy
-
-        c_copy = copy.copy(self._c)
-        r_copy = copy.deepcopy(self._r)
-        v_copy = copy.copy(self._v)
-
-        return Vocabulary(c_copy, r_copy, v_copy)
-
-    def constant_assignment(self, objs): 
+    @staticmethod
+    def constant_assignment(vocabulary, objs): 
         """
         Create a partial or total constant assignment from 
         vocab.C to objs.
         """
         
-        if not isinstance(objs, list):                                              #enforce objs parameter as list
-            raise TypeError(
-                'objs parameter must be of type list'
-                )
+        if not isinstance(objs, list):
+            raise TypeError('objs parameter must be of type list')
         
-        return pygame_mapping(self.get_C(), objs)
+        return pygame_mapping(vocabulary.get_C(), objs)
 
-    def variable_assignment(self, objs): 
+    @staticmethod
+    def variable_assignment(vocabulary, objs): 
         """
         Create a total constant assignment from vocab.V to objs.
         """
         
-        if not isinstance(objs, list):                                              #enforce objs parameter as list
-            raise TypeError(
-                'objs parameter must be of type list'
-                )
+        if not isinstance(objs, list):
+            raise TypeError('objs parameter must be of type list')
         
-        return pygame_mapping(self.get_V(), objs)
-        
-    def check_conflict(self, p1, p2):
-        """
-        Return boolean for whether or not p2 is in conflict with p1 in
-        respect to this Vocabulary.
-        """
-
-        dom1 = p1.get_domain()                                                      #grab Dom(p1)
-        dom2 = p2.get_domain()                                                      #grab Dom(p2)
-        intersection = list(set(dom1) & set(dom2))                                      #get intersection of Dom(p1) and Dom(p2)
-        
-        for c in intersection:
-            if p1.get_mapping()[c] != p2.get_mapping()[c]: 
-                return True                                                             #if any shared constants such that p1(c) != p2(c), conflict
-        return False                                                                    #otherwise, n
+        return pygame_mapping(vocabulary.get_V(), objs)
 
     def __str__(self):
+        """Implement str(Vocabulary)."""
         c_str = '[' + ''.join([c + ', ' for c in self.get_C()])[:-2] + ']'
         r_str = '[' + ''.join([str(r) + ', ' for r in self.get_R()])[:-2] + ']'
         v_str = '[' + ''.join([v + ', ' for v in self.get_V()])[:-2] + ']'
@@ -209,16 +120,12 @@ class Vocabulary:
         return '(' + c_str + ', ' + r_str + ', ' + v_str + ')'
 
     def __repr__(self):
-        c_str = '[' + ''.join([c + ', ' for c in self.get_C()])[:-2] + ']'
-        r_str = '[' + ''.join([str(r) + ', ' for r in self.get_R()])[:-2] + ']'
-        v_str = '[' + ''.join([v + ', ' for v in self.get_V()])[:-2] + ']'
-
-        return '(' + c_str + ', ' + r_str + ', ' + v_str + ')'
-
+        """Implement repr(Vocabulary)."""
+        return self.__str__()
 
 def main():
     """short tests."""
-
+    pass
 
 if __name__ == "__main__":
     main()
---
+++ Classes/test_Classes/test_Vocabulary.py
@@ -0,0 +1,29 @@
+"""Vocabulary unit tests."""
+
+import pytest
+from vivid.Classes.Vocabulary import Vocabulary
+
+def test___init__():
+    """."""
+    pass
+def test___eq__():
+    """."""
+    pass
+def test___ne__():
+    """."""
+    pass
+def test___deepcopy__():
+    """."""
+    pass
+def test_constant_assignment():
+    """."""
+    pass
+def test_variable_assignment():
+    """."""
+    pass
+def test___str__():
+    """."""
+    pass
+def test___repr__():
+    """."""
+    pass
